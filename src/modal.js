1023b1710eead1fecf2ebb7121ca3cf49651483d
slofurno <slofurno@gmail.com>
2016-05-22 15:59:27 -0400

    :cactus:

---
+++ src/modal.js
@@ -0,0 +1,61 @@
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+
+const _EditModal = ({clickHandler, dispatch}) => {
+  return (
+    <div>
+      <input type="button" value="save?" onClick={() => dispatch(clickHandler())}/>
+      lets edit something
+    </div>
+  )
+}
+
+const EditModal = connect((state, ownProps) => ownProps)(_EditModal)
+
+const SaveModal = ({}) => {
+  return (
+    <div>SAVED</div>
+  )
+}
+
+const modalLookup = {
+  "EDIT": EditModal,
+  "SAVE": SaveModal
+}
+
+const style = {
+  position: "fixed",
+  top: 0,
+  left: 0,
+  bottom: 0,
+  right: 0,
+  backgroundColor: "rgba(0,0,0,0.15)",
+  display: "flex",
+  padding: "50px"
+}
+
+const innerStyle = {
+  backgroundColor: "gainsboro",
+  flex: "1 0 auto"
+}
+
+class Modal extends Component {
+  render() {
+    const { modalProps, modalType } = this.props
+    if (!modalType) {
+      return null
+    }
+
+    const InnerModal = modalLookup[modalType]
+
+    return (
+      <div style={style}>
+        <div style={innerStyle}>
+          <InnerModal {...modalProps}/>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default connect(state => state.modal)(Modal)
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var webpack = require('webpack')
+
+module.exports = {
+
+  output: {
+    library: 'DumbModal',
+    libraryTarget: 'umd'
+  },
+
+  externals: [
+    {
+      react: {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react'
+      }
+    }
+  ],
+
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel' }
+    ]
+  },
+
+  plugins: [ ]
+
+}
